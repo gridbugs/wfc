5238d82af2cadbe15d49072415315a25e5b49e73
Stephen Sherratt <stephen@sherra.tt>
2020-02-14 23:37:47 +1100

    Remove some redundant extern crate lines

--- wfc/src/lib.rs
+++ wfc/src/lib.rs
@@ -1,10 +1,3 @@
-extern crate coord_2d;
-extern crate direction;
-extern crate grid_2d;
-extern crate hashbrown;
-extern crate rand;
-extern crate rayon;
-
 pub mod orientation;
 pub mod overlapping;
 pub mod retry;
